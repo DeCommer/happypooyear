5e910f6266bccc1dbf0ebfc81a3cc74c2064610a
Joseph DeCommer <jdecommer@gmail.com>
2019-07-20 11:46:20 -0400

    Added welcome message to change to month after x seconds on index page.

--- js/numberofpoos.js
+++ js/numberofpoos.js
@@ -1,1 +1,15 @@
 document.getElementById("numopoo").innerHTML = "147";
+
+var text = ["January"];
+var counter = 0;
+var elem = document.getElementById("hpy-text-1");
+var inst = setInterval(change, 4000);
+
+function change() {
+  elem.innerHTML = text[counter];
+  counter++;
+  if (counter >= text.length) {
+    counter = 0;
+    clearInterval(inst);
+  }
+}
