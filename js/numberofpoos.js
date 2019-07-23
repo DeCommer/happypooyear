1724d5922ed2b2a2376f0cd2c3d94b99c17a5585
Joseph DeCommer <jdecommer@gmail.com>
2019-07-23 13:52:12 -0400

    minor changes and tweaks

--- js/numberofpoos.js
+++ js/numberofpoos.js
@@ -1,15 +1,1 @@
 document.getElementById("numopoo").innerHTML = "151";
-
-var text = ["January"];
-var counter = 0;
-var elem = document.getElementById("hpy-text-1");
-var inst = setInterval(change, 3000);
-
-function change() {
-  elem.innerHTML = text[counter];
-  counter++;
-  if (counter >= text.length) {
-    counter = 0;
-    clearInterval(inst);
-  }
-}
