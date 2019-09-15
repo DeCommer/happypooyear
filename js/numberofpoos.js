1890a0e29de2c51f5a8f2f3dbcfcf454d6b401c3
Joseph DeCommer <jdecomer@gmail.com>
2019-09-15 18:24:28 -0400

    Changed javascript to log days left in 2019

--- js/numberofpoos.js
+++ js/numberofpoos.js
@@ -1,1 +1,5 @@
-document.getElementById("numopoo").innerHTML = "190";
+const date1 = new Date(('09/15/2019'));
+const date2 = new Date('01/01/2020');
+const diffTime = Math.abs(date2 - date1);
+const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+document.getElementById("numopoo").textContent = `190 / ${diffDays} days left`;
