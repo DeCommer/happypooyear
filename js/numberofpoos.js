1186f58f5a1d749015dc9936ac6b0b41b6a48097
Joseph DeCommer <jdecomer@gmail.com>
2019-12-31 10:45:27 -0500

    Last poo 123119

--- js/numberofpoos.js
+++ js/numberofpoos.js
@@ -2,4 +2,4 @@ const date1 = Date.now();
 const date2 = new Date('01/01/2020');
 const diffTime = Math.abs(date2 - date1);
 const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-document.getElementById("numopoo").textContent = `264 / ${diffDays} days left`;
+document.getElementById("numopoo").textContent = `266 / ${diffDays} days left`;
